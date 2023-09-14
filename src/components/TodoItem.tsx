87bfacd6b94279cc1aedb0c450cfe15661b643e0
zyryc15 <zyryc15@gmail.com>
2023-09-14 18:41:32 +0800

    initial change

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -14,27 +14,32 @@ export function TodoItem({
   deleteTodo,
 }: TodoItemProps) {
   return (
-    <li className="flex gap-1 items-center">
-      <input
-        id={id}
-        type="checkbox"
-        className="cursor-pointer peer"
-        defaultChecked={complete}
-        onChange={(e) => toggleTodo(id, e.target.checked)}
-      />
-      <label
-        htmlFor={id}
-        className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
-      >
-        {title}
-      </label>
-      <button
-        type="button"
-        className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded ml-5 mt-2"
-        onClick={() => deleteTodo(id)}
-      >
-        Delete
-      </button>
+    <li className="grid grid-cols-2 gap-3 items-center">
+      <div className="grid gap-x-1 grid-cols-2">
+        <input
+          id={id}
+          type="checkbox"
+          className="cursor-pointer peer"
+          defaultChecked={complete}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        <label
+          htmlFor={id}
+          className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
+        >
+          {title}
+        </label>
+      </div>
+
+      <div className="col-span-1">
+        <button
+          type="button"
+          className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded ml-5 mt-2"
+          onClick={() => deleteTodo(id)}
+        >
+          Delete
+        </button>
+      </div>
     </li>
   );
 }
